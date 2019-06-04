21aeb6219f74a27791d5172c55b33cd42dc5df53
OrganicMechanic <doum6708@vandals.uidaho.edu>
2019-06-04 12:13:33 -0700

    Assignment 1 upload

---
+++ assign1/test/token.h
@@ -0,0 +1,33 @@
+#ifndef TOKEN_H
+#define TOKEN_H 
+
+//=============  Structures  ====================
+typedef struct token {
+ int category;   /* the integer code returned by yylex */
+ char *text;     /* the actual string (lexeme) matched */
+ int lineno;     /* the line number on which the token occurs */
+ char *filename; /* the source file in which the token occurs */
+ int ival;       /* for integer constants, store binary value here */
+ double dval;	   /* for real constants, store binary value here */
+ char *sval;     /* for string constants, malloc space, de-escape, store */
+				/*    the string (less quotes and after escapes) here */
+ } *tokenP;		
+
+ typedef struct node{
+	 tokenP tokPtr;
+	 char *filename;
+	 struct node *next;
+ } *nodeP;
+ 
+//=============  Prototypes  ====================
+char *format_sval(char*);
+tokenP createTok(int, char*, int, char*) ;
+void popNode(nodeP*);
+void pushNode(nodeP*, char*);
+void addNode(nodeP*, tokenP);
+void printTok(tokenP);
+void printNode(nodeP*);
+void printStack(nodeP*);
+void clearNodes(nodeP*);
+
+#endif /* TOKEN_H */
---
+++ assign1/test/ytab.h
@@ -0,0 +1,52 @@
+/* Header file for g0.c */
+
+#define YY_BUFFER_SIZE 16384
+
+#define BREAK 258
+#define CLASS 259
+#define DOUBLE 261
+#define ELSE 262
+#define FL 263
+#define FOR 264
+#define IF 265
+#define INT 266
+#define LIST 267
+#define NUL 268
+#define RETURN 269
+#define STRING 270
+#define D 260
+#define TABLE 271
+#define TR 272
+#define VOID 273
+#define WHILE 274
+#define LPAREN 275 // (
+#define RPAREN 276 // )
+#define LSUBS 277 // [
+#define RSUBS 278 // ]
+#define BANG 279 //  !
+#define UNSIZE 280 // #
+#define MULT 281 // *
+#define DIV 282 // /
+#define MOD 283 // %
+#define PLUS 285 // +
+#define MINUS 286 // -
+#define LESS 287 // <
+#define LESSEQ 288 // <=
+#define GREATER 289 // >
+#define GREATEREQ 290 // >=
+#define EQUAL 291 // ==
+#define NOTEQ 292 // !=
+#define LOGAND 293 // &&
+#define LOGOR 294 // ||
+#define ASSIGN 295 // =
+#define INCREMENT 296 // +=
+#define DECREMENT 297 // -=
+#define SWAP 298 // :=:
+#define LCUR 299 // {
+#define RCUR 300 // }	
+#define QUOTE 301 // "
+#define IDENTIFIER 302
+#define ICON 303 // integer constant
+#define DCON 304 // double constant
+#define SEMIC 305 // ;
+#define BOOL 306 // T/F
